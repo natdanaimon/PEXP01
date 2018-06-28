d5ece68b74d29788aa474c936a8c1c564651c8fc
natdanaimon <natdanaimon@gmail.com>
2018-06-28 13:32:52 +0700

    fix

--- index.php
+++ index.php
@@ -38,10 +38,10 @@ $_SESSION['CONTEXT'] = $contextPath;
     </head>
     <!-- end::Head -->
     <!-- end::Body -->
-    <body  class="m--skin- m-header--fixed m-header--fixed-mobile m-aside-left--enabled m-aside-left--skin-dark m-aside-left--fixed m-aside-left--offcanvas m-footer--push m-aside--offcanvas-default"  >
+    <body class="m--skin- m-header--fixed m-header--fixed-mobile m-aside-left--enabled m-aside-left--skin-dark m-aside-left--offcanvas m-footer--push m-aside--offcanvas-default"  >
         <!-- begin:: Page -->
         <div class="m-grid m-grid--hor m-grid--root m-page">
-            <div class="m-grid__item m-grid__item--fluid m-grid m-grid--hor m-login m-login--signin m-login--2 m-login-2--skin-3" id="m_login" style="background-image: url(assets/app/media/img/bg/bg-2.jpg);">
+            <div class="m-grid__item m-grid__item--fluid m-grid m-grid--hor m-login m-login--signin m-login--2 m-login-2--skin-2" id="m_login" style="background-image: url(assets/app/media/img//bg/bg-3.jpg);">
                 <div class="m-grid__item m-grid__item--fluid	m-login__wrapper">
                     <div class="m-login__container">
                         <div class="m-login__logo">
@@ -52,62 +52,56 @@ $_SESSION['CONTEXT'] = $contextPath;
                         <div class="m-login__signin">
                             <div class="m-login__head">
                                 <h3 class="m-login__title">
-                                    Management
+                                    <!-- 									Innovation Hub -->
                                 </h3>
                             </div>
-                            <form class="m-login__form m-form" id="form-action">
+                            <form class="m-login__form m-form" id="form-action" >
+                                
                                 <div class="form-group m-form__group">
-                                    <input class="form-control m-input"   type="text" placeholder="ชื่อผู้ใช้งาน" id="user" name="user" autocomplete="off">
+                                    <input class="form-control m-input"   type="text" placeholder="ชื่อผู้ใช้งาน" name="user" id="user" autocomplete="off">
                                 </div>
                                 <div class="form-group m-form__group">
-                                    <div class="m-input-icon m-input-icon--right">
-                                        <input class="form-control m-input m-login__form-input--last" type="password" placeholder="รหัสผ่าน" id="pass" name="pass">
-                                        <span class="m-input-icon__icon m-input-icon__icon--right">
-                                            <span>
-<!--                                                <i class="la la-eye"></i>-->
-                                            </span>
-                                        </span>
-                                    </div>
+                                    <input class="form-control m-input m-login__form-input--last" type="password" placeholder="รหัสผ่าน" name="pass"  id="pass" >
                                 </div>
                                 <div class="row m-login__form-sub">
                                     <div class="col m--align-left m-login__form-left">
-                                        <label class="m-checkbox  m-checkbox--light">
+                                        <label class="m-checkbox  m-checkbox--focus">
                                             <input type="checkbox" name="remember">
                                             จดจำ
                                             <span></span>
                                         </label>
                                     </div>
-                                    <div class="col m--align-right m-login__form-right">
-                                        <!--                                        <a href="javascript:;" id="m_login_forget_password" class="m-link">
-                                                                                    ลืมรหัสผ่าน
-                                                                                </a>-->
-                                    </div>
+                                 
                                 </div>
                                 <div class="m-login__form-action">
-                                    <a onclick="login();" class="btn btn-focus m-btn m-btn--pill m-btn--custom m-btn--air  m-login__btn" style="color: white">
+                                    <a  href="javascript:login();" class="btn btn-focus m-btn m-btn--pill m-btn--custom m-btn--air m-login__btn m-login__btn--primary"
+                                           >
                                         เข้าสู่ระบบ
                                     </a>
                                 </div>
                             </form>
                         </div>
-
-                     
                         <div class="m-login__account">
                             <span class="m-login__account-msg">
                                 Copyright © 2018 EXP Webdesign All rights reserved.
                             </span>
-                            
-                            
+
+
                         </div>
+
                     </div>
                 </div>
             </div>
         </div>
         <!-- end:: Page -->
-
         <!--begin::Page Snippets -->
         <script src="js/authentication/login.js" type="text/javascript"></script>
         <!--end::Page Snippets -->
+
+
+
+
+
     </body>
     <!-- end::Body -->
 </html>
